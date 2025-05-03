e01452587f0988ddb3d9849bbbacb2e0ca04423d
Bodede Sodiq <younghustler252@gmail.com>
2025-05-04 00:39:46 +0100

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,26 @@
 const navLink = document.getElementById("navbar");
 const menuIcon = document.querySelector(".menu-icon i");
-function showMenu() {
-    navLink.classList.toggle('active')
+// const overlay = document.getElementById('overlay');
 
-    if (navLink.classList.contains('active')) {
-        
-        menuIcon.classList.remove('fa-bars')
-        menuIcon.classList.add('fa-times') 
-    } else {
-        menuIcon.classList.remove('fa-times')
-        menuIcon.classList.add('fa-bars')
-        
-    }
-}
 
+menuIcon.addEventListener('click', function () {
+    navLink.classList.toggle("active");
+    menuIcon.classList.toggle("fa-times");
+    menuIcon.classList.toggle("fa-bars");
+    // overlay.classList.toggle('active');
+})
+
+document.querySelectorAll('#navbar a').forEach(link => {
+    link.addEventListener('click', () => {
+      navLink.classList.remove('active');
+      menuIcon.classList.remove('fa-times');
+      menuIcon.classList.add('fa-bars');
+    });
+});
+// overlay.addEventListener('click', function () {
+//     navLink.classList.remove("active");
+//     menuIcon.classList.remove("fa-times");
+//     menuIcon.classList.add("fa-bars");
+//     overlay.classList.remove('active');
+    
+// })
